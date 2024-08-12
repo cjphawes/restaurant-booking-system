a7c3534499713dd4278fd606936b30a8d53e4201
Cameron Hawes <cameronhawes98@gmail.com>
2024-08-12 13:41:25 +0000

    feat:add CustomerAdmin class & adjust models with migrations

--- reservations/admin.py
+++ reservations/admin.py
@@ -5,9 +5,12 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Reservation)
 class ReservationAdmin(SummernoteModelAdmin):  
     list_display = ('customer', 'reservation_id', 'reservation_date')
-    search_fields = (['customer'])
+    search_fields = (['customer__full_name__icontains', 'reservation_date'])
     list_filter = ('customer', 'reservation_date',)
     
 
-# Register your models here.
-admin.site.register(Customer)
+@admin.register(Customer)
+class CustomerAdmin(SummernoteModelAdmin):  
+    list_display = ('full_name', 'email_address')
+    search_fields = (['full_name'])
+    list_filter = ('full_name',)
--- reservations/models.py
+++ reservations/models.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Customer(models.Model):
     customer_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=100)
     email_address = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15)
 
@@ -13,12 +12,12 @@ class Customer(models.Model):
     Function to display the parameters in the admin panel for a Customer
     """
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.full_name}"
 
 
 class Reservation(models.Model):
     reservation_id = models.AutoField(primary_key=True)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="client")
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="client_name")
     reservation_time = models.TimeField()
     reservation_date = models.DateField()
     number_of_guests = models.IntegerField(default=1)
