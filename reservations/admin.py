189102bee2b47ad1a82db8be1c66d96a04d61cf6
Cameron Hawes <cameronhawes98@gmail.com>
2024-08-12 12:27:51 +0000

    feat:add f strings to model funcs, migrate database changes & create html template

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Customer, Reservation
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Reservation)
+class ReservationAdmin(SummernoteModelAdmin):  
+    list_display = ('customer', 'reservation_id', 'reservation_date')
+    search_fields = (['customer'])
+    list_filter = ('customer', 'reservation_date',)
+    
 
 # Register your models here.
 admin.site.register(Customer)
-admin.site.register(Reservation)
--- reservations/models.py
+++ reservations/models.py
@@ -4,20 +4,21 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Customer(models.Model):
     customer_id = models.AutoField(primary_key=True)
-    username = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email_address = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15)
 
-
+    """
+    Function to display the parameters in the admin panel for a Customer
+    """
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
 class Reservation(models.Model):
     reservation_id = models.AutoField(primary_key=True)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="client")
     reservation_time = models.TimeField()
     reservation_date = models.DateField()
     number_of_guests = models.IntegerField(default=1)
@@ -26,6 +27,8 @@ class Reservation(models.Model):
     class Meta:
         ordering = ["-reservation_id"]
 
-
+    """
+    Function to display the parameters in the admin panel for a Reservation
+    """
     def __str__(self):
         return f"Reservation for {self.number_of_guests} at {self.reservation_time} on {self.reservation_date} for {self.customer}"
