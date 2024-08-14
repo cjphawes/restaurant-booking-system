e4990c41f2eea7a2f9d3479688642bd808db3225
Cameron Hawes <cameronhawes98@gmail.com>
2024-08-14 14:45:17 +0000

    feat:add & wire up the about & faq html templates

--- home/urls.py
+++ home/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, AboutView, FaqView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home")
+    path('home/', HomeView.as_view(), name="home"),
+    path('about/', AboutView.as_view(), name="about"),
+    path('faq/', FaqView.as_view(), name="faq")
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,11 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'home/index.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'home/about.html'
+
+
+class FaqView(TemplateView):
+    template_name = 'home/faq.html'
