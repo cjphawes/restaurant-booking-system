8b7f0c404421ac05ebebc192a5a8194fb4d8d47a
Cameron Hawes <cameronhawes98@gmail.com>
2024-09-19 14:58:03 +0000

    feat:add function for posting the form

--- reservations/views.py
+++ reservations/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib import messages
+from .models import Review, Reservation, Customer, User
 
 # Create your views here.
 class ReservationView(TemplateView):
@@ -13,3 +14,16 @@ class ContactView(TemplateView):
 
 class MenuView(TemplateView):
     template_name = 'reservations/menu.html'
+
+
+def review_form(request):
+    if request.method =='POST':
+        reviewer_email = request.POST.get('reviewer_email')
+        reviewer_name = request.POST.get('reviewer_name')
+        subject = request.POST.get('subject')
+        review_date = request.POST.get('review_date')
+        review_content = request.POST.get('review_content')
+
+
+
+    return render(request,)
