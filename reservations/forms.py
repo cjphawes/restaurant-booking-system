ddca9cae2f32534dc4050e47fb46ac29f750208d
Cameron Hawes <cameronhawes98@gmail.com>
2024-09-19 14:55:39 +0000

    feat:add form for review

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Reservation
+from .models import Reservation, Review
 
 
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
-        fields = ['Number of guests', 'Date', 'Time']
+        fields = ['Number of guests', 'Date', 'Time',]
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['Email Address', 'Name', 'Subject', 'Review Content']
