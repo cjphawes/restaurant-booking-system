7ba20afabf4a3c75a329869a55043602c2dd765e
Cameron Hawes <cameronhawes98@gmail.com>
2024-09-19 13:50:47 +0000

    feat:register review model in the admin panel

--- reservations/admin.py
+++ reservations/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.sites.models import Site
 from allauth.socialaccount.models import SocialToken, SocialAccount, SocialApp
 from allauth.account.models import EmailAddress
-from .models import Customer, Reservation
+from .models import Customer, Reservation, Review
 from django_summernote.admin import SummernoteModelAdmin
 from django_summernote.utils import get_attachment_model
 
@@ -19,6 +19,11 @@ class CustomerAdmin(SummernoteModelAdmin):
     search_fields = (['full_name'])
     list_filter = ('full_name',)
 
+
+@admin.register(Review)
+class FeedbackAdmin(SummernoteModelAdmin):
+    list_filter = ('review_date',)
+    search_fields = (['reviewer_name', 'subject'])
 
 # ADMIN PANELS THAT I WILL NOT NEED
 admin.site.unregister(Site)
