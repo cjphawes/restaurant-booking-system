fcba8ff5f4cce80d63244e994c2963b4739e8ca9
Cameron Hawes <cameronhawes98@gmail.com>
2024-09-23 19:55:55 +0000

    feat:add view for feedback form

--- reservations/views.py
+++ reservations/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from django.contrib import messages
 from .models import Review, Reservation, Customer, User
+from .forms import ReviewForm
 
 # Create your views here.
 class ReservationView(TemplateView):
@@ -12,18 +13,20 @@ class ContactView(TemplateView):
     template_name = 'reservations/contact.html'
 
 
+class FeedbackFormView(FormView):
+    """
+    Function to display form created in django
+    """
+    def review(request):
+        if request.method =='POST':
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                form.save()
+        else:
+            form = ReviewForm()
+                
+        return render(request, 'contact.html', {'review_form': form})
+
+
 class MenuView(TemplateView):
     template_name = 'reservations/menu.html'
-
-
-def review_form(request):
-    if request.method =='POST':
-        reviewer_email = request.POST.get('reviewer_email')
-        reviewer_name = request.POST.get('reviewer_name')
-        subject = request.POST.get('subject')
-        review_date = request.POST.get('review_date')
-        review_content = request.POST.get('review_content')
-
-
-
-    return render(request,)
