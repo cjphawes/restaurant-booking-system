855d1fb308edb112eef0217b2339c4c4460d2896
Cameron Hawes <cameronhawes98@gmail.com>
2024-10-07 14:15:37 +0000

    feat:remove customer model

--- reservations/models.py
+++ reservations/models.py
@@ -4,18 +4,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
-class Customer(models.Model):
-    full_name = models.CharField(max_length=100)
-    email_address = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15)
-
-    """
-    Function to display the parameters in the admin panel for a Customer
-    """
-    def __str__(self):
-        return f"{self.full_name}"
-
-
 TIME_PERIODS = (
     (0, '17:00'),
     (1, '17:30'),
@@ -34,7 +22,7 @@ TIME_PERIODS = (
 
 
 class Reservation(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="client_name")
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="client_name")
     reservation_time = models.IntegerField(choices=TIME_PERIODS, default=0)
     reservation_date = models.DateField()
     number_of_guests = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(20)])
@@ -49,18 +37,18 @@ class Reservation(models.Model):
         return f"Reservation for {self.number_of_guests} at {self.reservation_time} on {self.reservation_date} for {self.customer}"
 
 
-class Review(models.Model):
-    reviewer_email = models.EmailField(unique=True)
-    reviewer_name = models.CharField(max_length=250)
-    subject = models.CharField(max_length=250)
-    review_date = models.DateField()
-    review_content = models.TextField()
+# class Review(models.Model):
+#     reviewer_email = models.EmailField(unique=True)
+#     reviewer_name = models.CharField(max_length=250)
+#     subject = models.CharField(max_length=250)
+#     review_date = models.DateField()
+#     review_content = models.TextField()
 
-    class Meta:
-        ordering = ["review_date"]
+#     class Meta:
+#         ordering = ["review_date"]
 
-    """
-    Function to display the parameters in the admin panel for the reviews
-    """
-    def __str__(self):
-        return f"{self.reviewer_name} sent a review about '{self.subject}'"
+#     """
+#     Function to display the parameters in the admin panel for the reviews
+#     """
+#     def __str__(self):
+#         return f"{self.reviewer_name} sent a review about '{self.subject}'"
