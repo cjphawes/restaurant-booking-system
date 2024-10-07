f30899b0f7b1cb5d94fa11137fed04e77dc22df7
Cameron Hawes <cameronhawes98@gmail.com>
2024-10-07 15:11:57 +0000

    feat:change the models for reservation & review

--- reservations/models.py
+++ reservations/models.py
@@ -23,6 +23,8 @@ TIME_PERIODS = (
 
 class Reservation(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="client_name")
+    email_address = models.EmailField(unique=True)
+    name = models.CharField(max_length=150)
     reservation_time = models.IntegerField(choices=TIME_PERIODS, default=0)
     reservation_date = models.DateField()
     number_of_guests = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(20)])
@@ -37,18 +39,18 @@ class Reservation(models.Model):
         return f"Reservation for {self.number_of_guests} at {self.reservation_time} on {self.reservation_date} for {self.customer}"
 
 
-# class Review(models.Model):
-#     reviewer_email = models.EmailField(unique=True)
-#     reviewer_name = models.CharField(max_length=250)
-#     subject = models.CharField(max_length=250)
-#     review_date = models.DateField()
-#     review_content = models.TextField()
+class Review(models.Model):
+    reviewer_email = models.EmailField(unique=True)
+    reviewer_name = models.CharField(max_length=150)
+    subject = models.CharField(max_length=250)
+    review_date = models.DateField()
+    review_content = models.TextField()
 
-#     class Meta:
-#         ordering = ["review_date"]
+    class Meta:
+        ordering = ["review_date"]
 
-#     """
-#     Function to display the parameters in the admin panel for the reviews
-#     """
-#     def __str__(self):
-#         return f"{self.reviewer_name} sent a review about '{self.subject}'"
+    """
+    Function to display the parameters in the admin panel for the reviews
+    """
+    def __str__(self):
+        return f"{self.reviewer_name} sent a review about '{self.subject}'"
