91d6db3a701aadae6efcfeb910fa92fb18a6e8f6
Cameron Hawes <cameronhawes98@gmail.com>
2024-10-10 10:37:20 +0000

    fix:correct the urls.py to the reservations page so it now shows the list of reservation a user has made

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ReservationView, ContactView, MenuView, ReviewView, ReservationDetailsView, leave_review, make_reservation, RemoveReservationView, UpdateReservationView, list_reservations
+from .views import ReservationView, ContactView, MenuView, ReviewView, leave_review, make_reservation, RemoveReservationView, UpdateReservationView, list_reservations
 
 urlpatterns = [
-    path("reservation/", ReservationView.as_view(), name="reservation"),
-    path("your_reservation_details/", ReservationDetailsView.as_view(), name="reservationDetails"),
     path("contact_us/", ContactView.as_view(), name="contact"),
     path("leave_a_review/", ReviewView.as_view(), name="reviewView"),
-    path("our_menu/", MenuView.as_view(), name="menu"),
-    path("submit_review/", leave_review, name="submit_review"),
     path("make_reservation/", make_reservation, name="reservationForm"),
-    path("remove_reservation/", RemoveReservationView.as_view(), name="reservationRemove"),
-    path("update_reservation/", UpdateReservationView.as_view(), name="reservationUpdate"),
-    path("your_reservations/", list_reservations, name="reservationInfo"),
+    path("our_menu/", MenuView.as_view(), name="menu"),
+    path("reservation/", ReservationView.as_view(), name="reservation"),
+    path("remove_reservation/<int:id>/", RemoveReservationView.as_view(), name="reservationRemove"),
+    path("submit_review/", leave_review, name="submit_review"),
+    path("update_reservation/<int:id>/", UpdateReservationView.as_view(), name="reservationUpdate"),
+    path("your_reservation_details/", list_reservations, name="reservationDetails"),
 
 ]
