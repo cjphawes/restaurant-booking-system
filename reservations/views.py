c9168b10a32995e8c9476ab511b2e448cabb2e92
Cameron Hawes <cameronhawes98@gmail.com>
2024-10-10 07:56:21 +0000

    feat:add template views and func to display list of reservations

--- reservations/views.py
+++ reservations/views.py
@@ -1,28 +1,53 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView, FormView
+from django.views import View
 from django.contrib import messages
 from .models import Review, Reservation, User
 from datetime import datetime
 
-# Create your views here.
+
+
 class ReservationView(TemplateView):
     template_name = 'reservations/reservations.html'
+
 
 
 class ReservationDetailsView(TemplateView):
     template_name = 'reservations/view_reservation.html'
 
 
+
+class RemoveReservationView(TemplateView):
+    template_name = 'reservations/delete_reservation.html'
+
+
+
+class UpdateReservationView(TemplateView):
+    template_name = 'reservations/update_reservation.html'
+
+
+
 class ContactView(TemplateView):
     template_name = 'reservations/contact.html'
+
 
 
 class MenuView(TemplateView):
     template_name = 'reservations/menu.html'
 
 
+
 class ReviewView(TemplateView):
     template_name = 'reservations/review.html'
+
+
+
+@login_required
+def list_reservations(request):
+    reservations = Reservation.objects.all()
+    return render(request, 'reservations/view_reservations.html', {'reservations': reservations})
+
 
 """
 Function to POST review forms to the database
@@ -45,15 +70,18 @@ def leave_review(request):
 
     return render(request, "reservations/review.html")
 
+
+"""
+Function to create a reservation to the database
+"""
 def make_reservation(request):
     if request.method == "POST":
-        email = request.POST.get("email")
         name = request.POST.get("name")
         guests = request.POST.get("num_of_guests")
         time = request.POST.get("time")
         date = request.POST.get("date")
 
-        if not name or not email or not date or not time or not guests:
+        if not name or not date or not time or not guests:
             messages.error(request, "Please complete all fields.")
         else:
             """
@@ -61,10 +89,13 @@ def make_reservation(request):
             """
             time_selected = datetime.strptime(time, '%H:%M').time()
 
-            reservation = Reservation(email_address=email, name=name, reservation_date=date, reservation_time=time_selected, number_of_guests=guests)
+            reservation = Reservation(customer=request.user, name=name, reservation_date=date, reservation_time=time_selected, number_of_guests=guests)
 
             reservation.save()
             messages.success(request, "You're all set! We look forward to you joining us.")
+
+            client_email = reservation.customer.email
+            print (f"{client_email}")
 
         return render(request, "reservations/reservations.html", {'messages': messages.get_messages(request)})
 
