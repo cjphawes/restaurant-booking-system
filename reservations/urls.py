1f4d84e0f01f2c7cee96a007098d6bc6f1bed449
Cameron Hawes <cameronhawes98@gmail.com>
2024-10-11 18:47:15 +0000

    feat:update url for updating reservations

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ReservationView, ContactView, MenuView, ReviewView, leave_review, make_reservation, RemoveReservationView, UpdateReservationView, list_reservations
+from .views import ReservationView, ContactView, MenuView, ReviewView, leave_review, make_reservation, list_reservations, update_reservation
 
 urlpatterns = [
     path("contact_us/", ContactView.as_view(), name="contact"),
@@ -8,9 +8,9 @@ urlpatterns = [
     path("make_reservation/", make_reservation, name="reservationForm"),
     path("our_menu/", MenuView.as_view(), name="menu"),
     path("reservation/", ReservationView.as_view(), name="reservation"),
-    path("remove_reservation/<int:id>/", RemoveReservationView.as_view(), name="reservationRemove"),
+    # path("remove_reservation/<int:reservation_id>/", RemoveReservationView.as_view(), name="reservationRemove"),
     path("submit_review/", leave_review, name="submit_review"),
-    path("update_reservation/<int:id>/", UpdateReservationView.as_view(), name="reservationUpdate"),
+    path("update_reservation/<int:reservation_id>/", update_reservation, name="reservationUpdate"),
     path("your_reservation_details/", list_reservations, name="reservationDetails"),
 
 ]
