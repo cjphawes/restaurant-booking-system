d70c2cd071fb7d1ad2505bb8292d2c9aeb1eb38f
Cameron Hawes <cameronhawes98@gmail.com>
2024-10-12 22:13:44 +0000

    fix:rename js file to be more semantic

---
+++ static/js/alerts.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    let alerts = document.querySelectorAll('.alert');
+
+    if (alerts) {
+        alerts.forEach(alert => {
+            setTimeout(function() {
+                alert.classList.add("fade");
+
+                setTimeout(function() {
+                    alert.remove();
+                }, 300);
+            }, 3000)
+        });
+    }
+};
+
--- static/js/reservation.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    let anyMessages = document.querySelector('.alert');
-
-    if (anyMessages) {
-        let modal = new bootstrap.Modal(document.getElementById('reservationModal'));
-        modal.show();
-    }
-};
-
