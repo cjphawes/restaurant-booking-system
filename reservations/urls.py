2dc516b25e5c779e07f9ef2f3fa413b1477caec7
Cameron Hawes <cameronhawes98@gmail.com>
2024-10-12 21:41:21 +0000

    feat:add url path for removing a reservation

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ReservationView, ContactView, MenuView, ReviewView, leave_review, make_reservation, list_reservations, update_reservation
+from .views import ReservationView, ContactView, MenuView, ReviewView, leave_review, make_reservation, list_reservations, update_reservation, remove_reservation
 
 urlpatterns = [
     path("contact_us/", ContactView.as_view(), name="contact"),
@@ -8,7 +8,7 @@ urlpatterns = [
     path("make_reservation/", make_reservation, name="reservationForm"),
     path("our_menu/", MenuView.as_view(), name="menu"),
     path("reservation/", ReservationView.as_view(), name="reservation"),
-    # path("remove_reservation/<int:reservation_id>/", RemoveReservationView.as_view(), name="reservationRemove"),
+    path("remove_reservation/<int:reservation_id>/", remove_reservation, name="reservationRemove"),
     path("submit_review/", leave_review, name="submit_review"),
     path("update_reservation/<int:reservation_id>/", update_reservation, name="reservationUpdate"),
     path("your_reservation_details/", list_reservations, name="reservationDetails"),
