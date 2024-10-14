72a8f7c2a299f7df23c300087bb23a75dd476a35
Cameron Hawes <cameronhawes98@gmail.com>
2024-10-14 06:46:51 +0000

    maint:collectstatic files for deployment

---
+++ staticfiles/js/alerts.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    let alerts = document.querySelectorAll('.alert');
+
+    if (alerts) {
+        alerts.forEach(alert => {
+            setTimeout(function() {
+                alert.classList.add("fade");
+
+                setTimeout(function() {
+                    alert.remove();
+                }, 300);
+            }, 3000);
+        });
+    }
+};
