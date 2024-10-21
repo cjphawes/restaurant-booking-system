f79da917744383b568f12f0ed85e21eb5f4398c0
Cameron Hawes <cameronhawes98@gmail.com>
2024-10-21 20:59:57 +0000

    feat:add test js file

---
+++ static/tests/js/alerts.test.js
@@ -0,0 +1,9 @@
+const alert_message = require("../../js/alerts.js")
+
+describe("Alert Messages Fade", () => {
+    describe("Login", () => {
+        test("Should make alert message dissappear in 3 seconds", () => {
+            expect()
+        })
+    })
+})
