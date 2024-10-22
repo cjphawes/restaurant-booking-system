03b0e01c782d3ef993a167e930b2206edcdca7a4
Cameron Hawes <cameronhawes98@gmail.com>
2024-10-22 00:42:44 +0000

    fix:reformatted code from JSHint

--- static/tests/js/alerts.test.js
+++ static/tests/js/alerts.test.js
@@ -1,7 +1,7 @@
 /** * @jest-environment jsdom */
 require('@testing-library/jest-dom');
 
-jest.useFakeTimers()
+jest.useFakeTimers();
 const alert_message = require("../../js/alerts.js");
 
 describe("Alert Messages Fade", () => {
@@ -18,8 +18,8 @@ describe("Alert Messages Fade", () => {
         // checking to see if the alert has faded
         const alert = document.querySelectorAll(".alert");
             expect(alert.length).toBe(0);
-    })
-})
+    });
+});
 
 describe("Alert Messages Fade", () => {
     test("Should make alert message dissappear in 3 seconds", () => {
@@ -36,11 +36,11 @@ describe("Alert Messages Fade", () => {
         const alert = document.querySelectorAll(".alert");
         alert.forEach(alert => {
             expect(alert.className).toContain("fade");
-        })
+        });
 
         // simulating the time advancing and element being removed
         jest.advanceTimersByTime(300);
         const alert_removed = document.querySelectorAll(".alert");
         expect(alert_removed.length).toBe(0);
-    })
-})
+    });
+});
